70fa657407704db7e7654fdd68fa3ad8b481e23f
FloydKT <djohnsoo@aol.com>
2018-02-19 09:18:04 -0600

    Add files via upload

---
+++ ThePieShop/src/ThePieShop/Controllers/PieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThePieShop.Models;
+
+namespace ThePieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository
+    }
+
+}
